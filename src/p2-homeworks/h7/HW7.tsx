93cdafb0059d70a524af1c7461f597c7fb41b5c9
Aleksandr_Saponchik <alexsapon@gmail.com>
2022-04-11 22:06:38 +0300

    homework №7

--- src/p2-homeworks/h7/HW7.tsx
+++ src/p2-homeworks/h7/HW7.tsx
@@ -1,16 +1,17 @@
 import React, {useState} from 'react'
 import SuperSelect from './common/c5-SuperSelect/SuperSelect'
 import SuperRadio from './common/c6-SuperRadio/SuperRadio'
+import styles from './HW7.module.css'
 
 const arr = ['x', 'y', 'z']
 
 function HW7() {
-    const [value, onChangeOption] = useState(arr[1])
+    const [value, onChangeOption] = useState<string>(arr[1])
 
     return (
         <div>
             <hr/>
-            homeworks 7
+            <h2>Homeworks 7</h2>
 
             {/*should work (должно работать)*/}
             <div>
