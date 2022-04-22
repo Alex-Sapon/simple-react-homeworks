ae6de5ef3a81994e48c7e745d1d5772387815a6d
Aleksandr_Saponchik <alexsapon@gmail.com>
2022-04-22 18:32:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/simple-react-homeworks/precache-manifest.c0cd388b2fa142122f2eab5ff6a86e98.js"
+  "/simple-react-homeworks/precache-manifest.538a3ddae4c74e425e08966c0eb6e308.js"
 );
 
 self.addEventListener('message', (event) => {
